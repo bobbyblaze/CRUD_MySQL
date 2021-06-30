885fa43e47d0965489dcf4620adbdaa2f214dec3
robert <robertblazek02@gmail.com>
2021-06-30 22:07:50 +0200

    pridanie triedy user_manager + jej zakladna struktura

---
+++ src/main/java/com/learn/REST/model/user_manager.java
@@ -0,0 +1,50 @@
+package com.learn.REST.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class user_manager {
+
+    @Id
+    @GeneratedValue
+
+    @Column(name="id")
+    private int id;
+
+    @Column(name="login name")
+    private String login_name;
+
+    @Column(name="password")
+    private String password;
+
+    public user_manager(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin_name() {
+        return login_name;
+    }
+
+    public void setLogin_name(String login_name) {
+        this.login_name = login_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
